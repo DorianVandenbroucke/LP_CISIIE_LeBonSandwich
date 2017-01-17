b6d6c2bb8cab951998746002976b9a528b580c44
Ichters Emma <ichters.emma@gmail.com>
2017-01-17 15:23:01 +0100

    Ajout Update/Delete/Pay -> Commande

--- projet1_le_bon_sandwich/index.php
+++ projet1_le_bon_sandwich/index.php
@@ -4,4 +4,4 @@ require_once("vendor/autoload.php");
 src\utils\AppInit::bootEloquent('conf/conf.ini');
 
 use src\controllers\CommandeController as CommandeController;
-var_dump(CommandeController::add(2));
+var_dump(CommandeController::updateCommande(1, "2017-02-02"));
