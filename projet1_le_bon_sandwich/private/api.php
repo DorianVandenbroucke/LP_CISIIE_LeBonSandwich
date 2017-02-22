0b1331e5893b9190e4f15fa151c87dc684909b69
DORIAN-PC\DorianHa <dorian.vandenbroucke@outlook.com>
2017-02-22 20:17:22 +0100

    conflits

--- projet1_le_bon_sandwich/private/api.php
+++ projet1_le_bon_sandwich/private/api.php
@@ -19,65 +19,6 @@ session_start();
 $container = new \Slim\Container($conf);
 $app = new \Slim\App($container);
 
-
-$app->put("/commandes/{id}[/]",function(Request $req, Response $resp, $args){
-     return (new CommandeController($this))->changerEtatCommande($req, $resp, $args);
-  });
-
-
-$app->get('/ingredients[/]', function(Request $req, Response $resp, $args){
-    $args["baseUrl"] = $req->getUri()->getBaseUrl();
-    return (new DashBoardController($this))->ListIngredients($req, $resp, $args);
-})->setName('ingredients');
-
-$app->get('/ingredients/add[/]', function(Request $req, Response $resp, $args){
-    $args["baseUrl"] = $req->getUri()->getBaseUrl();
-    return (new DashBoardController($this))->AddIngredient($req, $resp, $args);
-})->setName('addIngredient');
-
-$app->post('/ingredients[/]', function(Request $req, Response $resp, $args){
-    $args["parsedBody"] = $req->getParsedBody();
-    $args["baseUrl"] = $req->getUri()->getBaseUrl();
-    return (new DashBoardController($this))->AddIngredient($req, $resp, $args);
-});
-
-
-$app->get('/ingredients/delete/{id}[/]', function(Request $req, Response $resp, $args){
-    $args["baseUrl"] = $req->getUri()->getBaseUrl();
-    return (new DashBoardController($this))->DeleteIngredient($req, $resp, $args);
-})->setName('deleteIngredient');
-
-
-$app->get("/authentification[/]", function(Request $req, Response $resp, $args){
-    return (new DashBoardController($this))->authentificationForm($req, $resp, $args);
-});
-
-// $app->get("/commandes/{id}[/]",function(Request $req, Response $resp, $args){
-//      return (new CommandeController($this))->detailCommande($req, $resp, $args['id']);
-//   })->setName("commandes");
-//
-//
-// $app->get("/commandes[/]", function(Request $req, Response $resp, $args){
-//   if(isset($_GET['offset']) || isset($_GET['size']))
-//   {
-//      return(new CommandeController($this))->paginationListCommande($req, $resp, $args);
-//   }
-//  return (new CommandeController($this))->filtrageCommandes($req, $resp, $args);
-// })->setName('commandes');
-//
-//
-// $app->get('/ingredients[/]', function(Request $req, Response $resp, $args){
-//     return (new DashBoardController($this))->ListIngredients($req, $resp, $args);
-// });
-
-// $app->get("/authentification[/]", function(Request $req, Response $resp, $args){
-//     return (new DashBoardController($this))->authentificationForm($req, $resp, $args);
-// });
-//
-// $app->post("/authentification[/]", function(Request $req, Response $resp, $args){
-//     return (new DashBoardController($this))->authentificationVerify($req, $resp, $args);
-// });
-
 require('../src/routes/private_route.php');
 
 
